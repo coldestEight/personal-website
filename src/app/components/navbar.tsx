d667b6285a56036c77cb3bef57d0f5f52165c4f3
Shaheer Khan <khan56@uoguelph.ca>
2025-04-30 13:42:06 -0400

    refactor: changed how site routing was handled

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,37 +1,20 @@
 "use client";
+import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 import styles from "./navbar.module.css";
 
-function home() {
-  window.location.href = "../";
-}
+export default function Navbar() {
+  const router = useRouter();
+  const handleNavigation = (url: string) => {
+    router.push(url);
+  };
 
-function aboutMe() {
-  window.location.href = "../about_me";
-}
-
-function projects() {
-  window.location.href = "../projects";
-}
-
-function contact() {
-  window.location.href = "../contact";
-}
-
-function coop() {
-  window.location.href = "../co-op";
-}
-
-export default function navbar() {
   return (
-    <>
-      <div className={styles.navbar}>
-        <button onClick={() => home()}>Home</button>
-        <button onClick={() => aboutMe()}>About Me</button>
-        <button onClick={() => projects()}>Projects</button>
-        <button onClick={() => coop()}>Co-op</button>
-        <button onClick={() => contact()}>Contact</button>
-      </div>
-    </>
+    <div className={styles.navbar}>
+      <button onClick={() => handleNavigation("/")}>Home</button>
+      <button onClick={() => handleNavigation("/about_me")}>About Me</button>
+      <button onClick={() => handleNavigation("/projects")}>Projects</button>
+      <button onClick={() => handleNavigation("/contact")}>Contact</button>
+    </div>
   );
 }
